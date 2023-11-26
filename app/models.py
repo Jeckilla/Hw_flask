a45033766eb69637dc466eecdb951495d2e93a49
jeckilla <jeckilla@gmail.com>
2023-11-26 22:30:44 +0300

    Corrected commites history

--- app/models.py
+++ app/models.py
@@ -8,8 +8,7 @@ import datetime
 
 
 
-
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", 'postgres')
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", '123345')
 POSTGRES_USER = os.getenv("POSTGRES_USER", 'postgres')
 POSTGRES_DB = os.getenv("POSTGRES_DB", 'app')
 POSTGRES_HOST = os.getenv("POSTGRES_HOST", '127.0.0.1')
